fd403c5e94ec2dc4cd719732fce3281b2c8dd967
sdarshn <50020372+sdarshn@users.noreply.github.com>
2021-02-16 00:56:50 +0530

    //Added new style for Hint and Text (#1)
    Co-authored-by: darshn <sdarshan.sn@gmail.com>

--- app/HintEditText/src/main/java/dev/darshn/hintedittext/HintEditText.kt
+++ app/HintEditText/src/main/java/dev/darshn/hintedittext/HintEditText.kt
@@ -7,45 +7,63 @@ import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 
-class HintEditText : LinearLayout {
-    lateinit var tvHint: TextView
-    lateinit var etText: EditText
 
-    constructor(context: Context) : this(context, null){
+class HintEditText : LinearLayout {
+    private lateinit var tvHint: TextView
+    private lateinit var etText: EditText
+
+
+    constructor(context: Context) : this(context, null) {
         //initUI()
     }
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0) {
         //  initUI()
         getAttrValue(context, attrs)
     }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
         initUI()
         getAttrValue(context, attrs)
 
     }
 
 
-    private fun initUI(){
+    private fun initUI() {
         View.inflate(context, R.layout.layout_hint_text, this)
-        tvHint = findViewById<TextView>(R.id.tv_hint)
-        etText = findViewById<EditText>(R.id.et_text)
+        tvHint = findViewById(R.id.tv_hint)
+        etText = findViewById(R.id.et_text)
     }
 
-    private fun getAttrValue(context: Context, attr : AttributeSet?){
-        var typeedArray = context.obtainStyledAttributes(attr, R.styleable.HintEditText)
-        var attrValue = typeedArray.getString(R.styleable.HintEditText_hint)
-        if(attrValue!=null) {
-            tvHint.text= attrValue
+    private fun getAttrValue(context: Context, attr: AttributeSet?) {
+        val typeedArray = context.obtainStyledAttributes(attr, R.styleable.HintEditText)
+        val attrValue = typeedArray.getString(R.styleable.HintEditText_hint)
+        val hintColor = typeedArray.getColor(
+            R.styleable.HintEditText_hintColor,
+            resources.getColor(R.color.design_default_color_on_primary)
+        )
+        val tvColor = typeedArray.getColor(
+            R.styleable.HintEditText_textColor,
+            resources.getColor(R.color.design_default_color_on_primary)
+        )
+        if (attrValue != null) {
+            tvHint.text = attrValue
+            etText.setTextColor(tvColor)
+            tvHint.setTextColor(hintColor)
         }
     }
 
 
-    public fun setError(error : String){
-        etText?.setError(error)
+    fun setError(error: String) {
+        etText.error = error
     }
 
-    public fun setText(text: String){
-        etText?.setText(text)
+    fun setText(text: String) {
+        etText.setText(text)
     }
 }
 
